70350e7bc1615e6c3ebb9f5a15cb979757a697f7
wlehoapt <wlehoapt@gmail.com>
2021-06-29 23:46:11 +0900

    Add UnitTest Sample Project (with failed test sample.)

---
+++ UnitTest/TestCircularBuffer.cs
@@ -0,0 +1,14 @@
+﻿namespace UnitTest
+{
+  using Xunit;
+
+  public class TestCircularBuffer
+  {
+    [Theory]
+    [InlineData(0)]
+    public void CircularBuffer_Produce_CorrectContent(int value)
+    {
+      Assert.True(value == 1);
+    }
+  }
+}
